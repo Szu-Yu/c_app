47849aa82b208d2ff849aee74c7ab635192cbf28
CHU <Chu@10.232.197.47>
2015-04-08 00:55:57 +0800

    all stuffs

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,53 @@
+class CoursesController < ApplicationController
+	def index
+		@courses=Course.all
+		if params[:name].present?
+		   @courses=@courses.where("name like?","%#{params[:name]}%")
+		end	
+		if params[:teacher].present?
+		   @courses=@courses.where("name like?","%#{params[:teacher]}%")
+		end	
+		if params[:time].present?
+			@courses=@courses.where("name like?","%#{params[:time]}%")
+		end	
+	end	
+	def show
+		@course=Course.find(params[:id])
+	end
+	def new
+		@course=Course.new
+	end
+	def create 
+		@course=Course.new(course_params)
+		if @course.save 
+			redirect_to root_path
+		else
+			render:new
+		end	
+	end
+	def edit
+		@course=Course.find(params[:id])
+	end
+	def destroy
+		@course=Course.find(params[:id])
+		@course.destroy
+		redirect_to root_path
+	end
+	def update
+		@course=Course.find(params[:id])
+		if @course.update(course_params)
+			redirect_to root_path
+		else
+			render :edit
+		end
+	end
+
+	
+
+
+	private 
+		def course_params
+			params.require(:course).permit(:name, :teacher, :time)
+		end
+
+end
